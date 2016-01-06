d5558a1d3ef17f9a91061ecbf77545149af35657
Yugang Zhang <yuzhang@xf11id-ws1.cs.nsls2.local>
2016-01-06 16:39:32 -0500

    chx_att

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)
+
+import sys
+import warnings
+
+
+from setuptools import setup
+
+setup(
+    name='chx_atten',
+    version='0.0.0',
+    author='Brookhaven National Laboratory',
+    #packages=['chxtools', 'pyXPCS', 'chxtools.pims_readers'],
+    #package_data={'chxtools/X-ray_database' : ['.dat']},
+    #include_package_data=True,
+)
